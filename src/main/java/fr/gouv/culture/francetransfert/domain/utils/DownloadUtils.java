3a4ddbec6b2a17b43e0187c97e4934a4e5aef720
TAHTAH Hussein <EXT_HTA37@open-groupe.com>
2020-01-22 16:23:55 +0100

    commit changes for private constructors on utils classes

--- src/main/java/fr/gouv/culture/francetransfert/domain/utils/DownloadUtils.java
+++ src/main/java/fr/gouv/culture/francetransfert/domain/utils/DownloadUtils.java
@@ -4,6 +4,10 @@ import java.io.UnsupportedEncodingException;
 import java.util.Base64;
 
 public class DownloadUtils {
+	
+	private DownloadUtils() {
+		// private Constructor
+	}
 
     public static String base64Decoder(String string) throws UnsupportedEncodingException {
         byte[] asBytes = Base64.getUrlDecoder().decode(string);
