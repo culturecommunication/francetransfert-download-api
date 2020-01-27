bf17fd8d4b67473e2f55c93a4438ca6183773124
HADDAD LOUAY <LHA16980@open-groupe.com>
2020-01-27 09:58:16 +0100

    satisfaction download

---
+++ src/main/java/fr/gouv/culture/francetransfert/application/services/RateServices.java
@@ -0,0 +1,30 @@
+package fr.gouv.culture.francetransfert.application.services;
+
+import com.google.gson.Gson;
+import com.opengroup.mc.francetransfert.api.francetransfert_metaload_api.RedisManager;
+import com.opengroup.mc.francetransfert.api.francetransfert_metaload_api.enums.RedisQueueEnum;
+import fr.gouv.culture.francetransfert.application.resources.model.rate.RateRepresentation;
+import fr.gouv.culture.francetransfert.domain.exceptions.DownloadException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Service;
+
+@Service
+public class RateServices {
+    private static final Logger LOGGER = LoggerFactory.getLogger(RateServices.class);
+
+    public void createSatisfactionFT(RateRepresentation rateRepresentation) throws DownloadException {
+        try {
+            if (null == rateRepresentation) {
+                LOGGER.error("error satisfaction services");
+                throw new DownloadException("error satisfaction services");
+            }
+            String jsonInString = new Gson().toJson(rateRepresentation);
+
+            RedisManager redisManager = RedisManager.getInstance();
+            redisManager.publishFT(RedisQueueEnum.SATISFACTION_QUEUE.getValue(), jsonInString);
+        } catch (Exception e) {
+            throw new DownloadException("error satisfaction services");
+        }
+    }
+}
