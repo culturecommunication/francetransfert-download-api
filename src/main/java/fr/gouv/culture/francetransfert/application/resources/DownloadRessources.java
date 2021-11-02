fb6818daef6ae80fda7005e0120192255c7cd403
Julien Aubert <j.aubert@actongroup.com>
2021-11-02 18:08:34 +0100

    crossorigin

--- src/main/java/fr/gouv/culture/francetransfert/application/resources/DownloadRessources.java
+++ src/main/java/fr/gouv/culture/francetransfert/application/resources/DownloadRessources.java
@@ -8,7 +8,6 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -31,7 +30,6 @@ import fr.gouv.culture.francetransfert.model.RateRepresentation;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 
-@CrossOrigin
 @RestController
 @RequestMapping("/api-private/download-module")
 @Api(value = "Download resources")
